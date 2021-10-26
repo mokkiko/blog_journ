f18032ec1d2eed8c038e157cee7354c157c4baa0
mokkiko <maksim_pro@mail.ru>
2021-10-26 18:27:33 +0300

    add validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,22 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.valid?
+      @contact.save
+    else
+      render action: 'new'
+    end
+
   end
+
+  private
+
+  def contact_params
+    params.require(:contacts).permit(:email, :message)
+
+  end
+
+
 
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  validates :email, presence: true
+  validates :message, presence: true
+
 end
