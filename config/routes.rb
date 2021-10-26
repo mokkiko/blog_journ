51c58ae4564b8e6d04a04e73746908e1a540621a
mokkiko <maksim_pro@mail.ru>
2021-10-26 17:02:23 +0300

    add contacts form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :articles
   get 'article/new'
+  get 'home/contacts'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
