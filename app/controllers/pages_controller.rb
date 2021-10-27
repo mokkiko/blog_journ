aafe6b3b81292e66915d770754f413fe9bd305c8
mokkiko <maksim_pro@mail.ru>
2021-10-27 15:25:30 +0300

    add tems and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+  def terms
+
+  end
+
+
+  def about
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :articles
   get 'article/new'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
 
 
 
