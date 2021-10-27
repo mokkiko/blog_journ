27e6a10045f9ce2081ec6f5a1b4bd01247155ea7
mokkiko <maksim_pro@mail.ru>
2021-10-27 14:43:34 +0300

    implement article show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,25 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
+  end
+
+  def article_params
+    params.require(:articles).permit(:title, :text )
+
   end
 
 
